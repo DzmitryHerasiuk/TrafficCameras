0146c6d5d23cac6ebfaebab47b8da259c0f8277e
Dzmitry Herasiuk <dgerasuk@vtb.ru>
2021-06-16 23:04:06 +0300

    Create project

---
+++ TrafficCameras/TrafficCameras/TrafficCamerasApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficCamerasApp.swift
+//  TrafficCameras
+//
+//  Created by Dzmitry Herasiuk on 16.06.21.
+//
+
+import SwiftUI
+
+@main
+struct TrafficCamerasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
